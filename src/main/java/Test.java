5d110e3d6eb1f2d48d2b288c7b56fbbdde9de9d6
Pratik <pratikquora@gmail.com>
2018-05-28 21:56:39 -0400

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello to maven dependency project");
+        String string_which_is_to_be_reversed = StringUtils.reverse("ABC");
+        System.out.println(string_which_is_to_be_reversed);
+    }
+}
